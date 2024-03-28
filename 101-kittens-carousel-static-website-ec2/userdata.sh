44127a681c24cf8ca263b2b5815fb3bec9bbaf38
Juliet <asumaj57@gmail.com>
2024-03-28 10:46:53 -0400

    fix name

--- 101-kittens-carousel-static-website-ec2/userdata.sh
+++ 101-kittens-carousel-static-website-ec2/userdata.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
+# update the os
 yum update -y
+
+#install Apache
 yum install httpd -y
+
+# copy content to /var/www/thml folder
+cd /var/www/html
+FOLDER="https://raw.githubusercontent.com/EsAsuJu90/My-repository/main/101-kittens-carousel-static-website-ec2/static-web"
+wget ${FOLDER}/index.html
+wget ${FOLDER}/cat0.jpg
+wget ${FOLDER}/cat1.jpg
+wget ${FOLDER}/cat2.jpg
+
+# start and enable Apache service
+systemctl start httpd
+systemctl enable httpd
